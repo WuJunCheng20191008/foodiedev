932ff5cf76d6ae499503a6e0d3dda8654e642ead
Demon <2934123238@qq.com>
2020-09-09 07:13:40 +0800

    分类实现-自连接查询子分类

--- foodie-dev-service/src/main/java/com/imooc/service/CategoryService.java
+++ foodie-dev-service/src/main/java/com/imooc/service/CategoryService.java
@@ -2,6 +2,7 @@ package com.imooc.service;
 
 
 import com.imooc.pojo.Category;
+import com.imooc.pojo.vo.CategoryVO;
 
 import java.util.List;
 
@@ -10,4 +11,11 @@ public interface CategoryService {
     * 查询所有一级分类
     * */
     public List<Category> queryAllRootLevelCat();
+
+    /**
+     * 根据一级分类id查询子分类信息
+     * @param rootCatId
+     * @return
+     */
+    public List<CategoryVO> getSubCatList(Integer rootCatId);
 }
