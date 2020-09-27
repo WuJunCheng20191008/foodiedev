80828fe653de272821475eb13bdd320ecbc173b7
Demon <2934123238@qq.com>
2020-09-27 16:01:19 +0800

    定时任务需要优化（通过消息队列方式解决）

--- foodie-dev-api/src/main/java/com/imooc/config/OrderJob.java
+++ foodie-dev-api/src/main/java/com/imooc/config/OrderJob.java
@@ -13,6 +13,19 @@ public class OrderJob {
 
     @Autowired
     private OrderService orderService;
+    /*
+    * 使用定时任务关闭超期未支付订单，会存在弊端
+    *  1.会有时间差，程序不严谨
+    *       10.39下单，11：00检查不足一小时，12：00检查，超过一个小时多39分
+    *  2.不支持集群
+    *       单机可以，可以集群后，就会有多个定时任务
+    *       解决方法：只使用一台计算机节点，单独运行所有的定时任务
+    * 3.会对数据库全表搜索，非常影响数据库性能
+    *       定时任务，仅仅只适用于小型轻量级项目，传统项目
+    * 后续课程会涉及到消息队列：MQ->RabbitMQ,Kafka,ZeroMQ
+    *       延时任务（队列）
+    *       10.12分下单，未付款（10）状态，11：12分检查，如果当前订单还是10，则直接关闭订单
+    * */
     @Scheduled(cron = "0 0 0/1 * * ?")
     public void autoCloseOrder(){
         orderService.closeOrder();
